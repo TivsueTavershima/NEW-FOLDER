a3a1924700583781054d0ba50509a9234b4ba8ca
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-10-24 16:26:46 +0100

    changes

--- week_1/day_1.py
+++ week_1/day_1.py
@@ -9,3 +9,28 @@ else:
 cast = (1980)
 sentence = "i like rock music in " + str(cast) + "s"
 print(sentence)
+
+multiline = '''
+hello, how are you doing 
+                    just checking in!
+                                                                          all good.
+'''
+print(multiline)
+
+
+
+print(len(multiline))
+multiline +="                                                              "
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+print("")
+# build menu
+
+title = "menu".upper()
+print(title.center(20, "="))
+
+sentence = f"{"====" * 5}\nHello, how are you?\n{"====" * 5}"
+print(sentence)
+
+print("coffee".ljust(20, ".") + "$1".rjust(4))
